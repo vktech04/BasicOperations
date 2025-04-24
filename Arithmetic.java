6838073a9e7b6e731195c1ff63d5ca5f393eb63a
vktech04 <kulkarnivaishu9504@gmail.com>
2025-04-24 18:59:07 +0530

    saved in master

--- Arithmetic.java
+++ Arithmetic.java
@@ -1,9 +1,18 @@
 public class Arithmetic {
     public static void main(String[] args) {
-        int x = 20, y= 10;
+        int x = 31, y= 10;
         int sum = x + y;
         int sub = x - y;
+        int mult = x * y ;
         System.out.println("The Addition is : "+sum);
         System.out.println("The Subtraction is : "+sub);
+        System.out.println("The Multiplication is : "+mult);
+       if(y!=0){
+        int div = x/y;
+        System.out.println("The Division is : "+div);
+       }
+       else{
+        System.out.println("Cannot divide a number by zero.");
+       }
     }
 }

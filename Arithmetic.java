89ef761ad64f77e365973216e3539c20d032f8b7
vktech04 <kulkarnivaishu9504@gmail.com>
2025-04-24 18:49:42 +0530

    saved in main

---
+++ Arithmetic.java
@@ -0,0 +1,9 @@
+public class Arithmetic {
+    public static void main(String[] args) {
+        int x = 20, y= 10;
+        int sum = x + y;
+        int sub = x - y;
+        System.out.println("The Addition is : "+sum);
+        System.out.println("The Subtraction is : "+sub);
+    }
+}

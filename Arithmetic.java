e74026859b2a4239c72dfce25cab881bfc10991f
vktech04 <kulkarnivaishu9504@gmail.com>
2025-05-07 15:00:22 +0530

    saved in main

--- Arithmetic.java
+++ Arithmetic.java
@@ -3,12 +3,10 @@ public class Arithmetic {
         int x = 31, y= 10;
         int sum = x + y;
         int sub = x - y;
-        int div = x/y;
-        int mult = x * y ;
+        int prod = x * y ;
        
         System.out.println("The Addition is : "+sum);
         System.out.println("The Subtraction is : "+sub);
-        System.out.println("The Multiplication is : "+mult);
-        System.out.println("The Division is : "+div);
+        System.out.println("The Multiplication is : "+prod);
     }
 }
